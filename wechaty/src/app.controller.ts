d559e0d91c1b36ff8fa964f99125185e8cbf2401
邓邵华 <441636706@qq.com>
2024-11-20 23:06:02 +0800

    修改微信群聊机器人代码，demo1新增env.local配置文件

--- wechaty/src/app.controller.ts
+++ wechaty/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { WechatyBuilder } from 'wechaty';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,28 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('scan')
+  getScan(@Res() res) {
+    const bot = WechatyBuilder.build();
+    bot
+      .on('scan', async (qrcode, status) => {
+        const code = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
+        this.appService.generateQrCode(encodeURIComponent(qrcode));
+        // return { message: 'QR Code generated in terminal' };
+        res.send(code);
+        /* const qrCodeUrl = await this.appService.generateQrCode(code);
+        res.type('image/png');
+        res.send(qrCodeUrl); */
+      })
+      .on('login', (user) => console.log('登录成功：' + user))
+      .on('message', (message) => console.log('收到消息：' + message))
+      .on('friendship', (friendship) =>
+        console.log('收到好友请求：' + friendship),
+      )
+      .on('room-invite', (invitation) =>
+        console.log('收到入群邀请：' + invitation),
+      )
+      .start();
   }
 }
--- wechaty/src/app.service.ts
+++ wechaty/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
-
+import * as QRCode from 'qrcode';
+import * as qrcodeTerminal from 'qrcode-terminal';
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  /* async generateQrCode(data: string): Promise<Buffer> {
+    try {
+      return await QRCode.toBuffer(data);
+    } catch (err) {
+      throw new Error('Failed to generate QR code');
+    }
+  } */
+  generateQrCode(data: string) {
+    qrcodeTerminal.generate(data, { small: true }, (code) => {
+      console.log(code);
+    });
   }
 }
